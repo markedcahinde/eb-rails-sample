95a8dca84c27a9ccb7c7246addabcb507647bd52
EC2 Default User <ec2-user@ip-172-31-33-0.eu-west-2.compute.internal>
2023-01-12 14:34:13 +0000

    set up local caching of gems and node modules

--- .platform/hooks/prebuild/00_config.sh
+++ .platform/hooks/prebuild/00_config.sh
@@ -3,4 +3,11 @@
 set -x
 
 amazon-linux-extras install postgresql14
+mkdir -p /var/app/build/gems
+chown webapp: /var/app/build/gems
+ls -sf /var/app/build/gems ./build/gems
 bundle config set --local path build/gems
+
+mkdir -p /var/app/node_modules
+chown webapp: /var/app/node_modules
+ln -sf /var/app/node_modules ./node_modules
