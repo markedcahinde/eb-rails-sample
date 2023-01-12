e3bf5fcbcf983bc2c959cfeadd02e592cae15b65
EC2 Default User <ec2-user@ip-172-31-33-0.eu-west-2.compute.internal>
2023-01-12 14:46:56 +0000

    update paths in platform hook

--- .platform/hooks/prebuild/00_config.sh
+++ .platform/hooks/prebuild/00_config.sh
@@ -5,8 +5,8 @@ set -x
 amazon-linux-extras install postgresql14
 mkdir -p /var/app/build/gems
 chown webapp: /var/app/build/gems
-ls -sf /var/app/build/gems ./build/gems
-bundle config set --local path build/gems
+ls -sf /var/app/build/gems ./vendor/bundle
+bundle config set --local path vendor/bundle
 
 mkdir -p /var/app/node_modules
 chown webapp: /var/app/node_modules
