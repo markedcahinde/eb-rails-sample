d6907d95d1613b215f7e43f27bc0593b4287e0ab
Mark Cahinde <mark@CB-L022.local>
2023-01-13 18:13:32 +0800

    adds predeploy hook

---
+++ .platform/hooks/predeploy/00_fix_bundle_permission_redo_rake.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+/bin/sh -c rbenv global
+
+chown -R webapp: /var/app/build/gems || echo "run failed"
+chown -R webapp: /var/app/node_modules || echo "run failed"
+
+rails_skip_asset_compilation=$(/opt/elasticbeanstalk/bin/get-config environment -k RAILS_SKIP_ASSET_COMPILATION)
+rails_skip_migrations=$(/opt/elasticbeanstalk/bin/get-config environment -k RAILS_SKIP_MIGRATIONS)
+
+if [[ $rails_skip_asset_compilation == "false" ]] || [[ -z $rails_skip_asset_compilation ]]; then
+  /bin/su webapp -c bundle exec /opt/elasticbeanstalk/config/private/checkforraketask.rb assets:precompile
+fi
+
+if [[ $rails_skip_migrations == "false" ]] || [[ -z $rails_skip_migrations ]]; then
+  /bin/su webapp -c bundle exec /opt/elasticbeanstalk/config/private/checkforraketask.rb db:migrate
+fi
